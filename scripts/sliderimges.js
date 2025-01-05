d8ed8841e85ee9be7d7e151796f92b850b7b660f
SarojKanvadiya <sarojkanvadiya@gmail.com>
2025-01-05 10:15:46 +0530

    slidimg

--- scripts/sliderimges.js
+++ scripts/sliderimges.js
@@ -1,7 +1,7 @@
-const sliderImages = `<img src="./assets/medical-banner-with-doctor-wearing-goggles.jpg" alt="Image 1">
-        <img src="./assets/DALL·E 2024-12-31 14.33.51 - A professional slider image for a medical product website, designed for a promotional campaign. The first image features a clean a.webp"
-            alt="Image 2">
-        <img src="./assets/medical-banner-with-doctor-wearing-goggles.jpg" alt="Image 3">`;
+const sliderImages = `<img src="./assest/medical-banner-with-doctor-wearing-goggles.jpg" alt="Image 1">
+<img src="./assest/medical-banner-with-doctor-wearing-goggles.jpg"
+    alt="Image 2">
+<img src="./assest/medical-banner-with-doctor-wearing-goggles.jpg" alt="Image 3">`;
 
 const cont = document.getElementById("slider");        
 cont.innerHTML = sliderImages;
